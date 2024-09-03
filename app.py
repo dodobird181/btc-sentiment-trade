8037a59b9ff26badc9758f9f4003c5c78b0dfae3
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2024-09-03 18:54:23 -0400

    JOEY COMMIT

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import os
+import inspect
 import strategy
 
 print('Starting app...')
 
 if strategy_clsname := os.getenv('BACKTEST'):
     # app is running in backtesting mode
-    strategy_cls = getattr(strategy, strategy_clsname)
-    strategy = strategy_cls()
+    try:
+        strategy_cls = getattr(strategy, strategy_clsname)
+        strategy = strategy_cls()
+    except AttributeError:
+        print(
+            f'Could not find backtesting strategy "{strategy_clsname}". ' +
+            f"Currently defined strategies are: {[
+                name for name, cls in inspect.getmembers(strategy, inspect.isclass)
+                if cls.__module__ == strategy.__name__
+            ]}."
+        )
